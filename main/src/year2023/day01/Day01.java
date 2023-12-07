8f687f8a6a1723db815e60f19807da77d436bcfb
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-12-07 01:03:25 +0100

    optimize day01

--- main/src/year2023/day01/Day01.java
+++ main/src/year2023/day01/Day01.java
@@ -2,9 +2,9 @@ package year2023.day01;
 
 public class Day01 {
     public static void main(String[] args) {
+        String[] inputs = Input.INPUT.replaceAll("[a-z]+", "").split("\n");
         long result = 0;
-        for (String input : Input.INPUT) {
-            input = input.replaceAll("[a-z]", "");
+        for (String input : inputs) {
             result += Long.parseLong(input.charAt(0) + "") * 10 + Long.parseLong(input.charAt(input.length() - 1) + "");
         }
         System.out.println(result);
--- main/src/year2023/day01/Day01_extra.java
+++ main/src/year2023/day01/Day01_extra.java
@@ -2,19 +2,21 @@ package year2023.day01;
 
 public class Day01_extra {
     public static void main(String[] args) {
-        long result = 0;
-        for (String input : Input.INPUT) {
-            input = input.replaceAll("one", "one1one");
-            input = input.replaceAll("two", "two2two");
-            input = input.replaceAll("three", "three3three");
-            input = input.replaceAll("four", "four4four");
-            input = input.replaceAll("five", "five5five");
-            input = input.replaceAll("six", "six6six");
-            input = input.replaceAll("seven", "seven7seven");
-            input = input.replaceAll("eight", "eight8eight");
-            input = input.replaceAll("nine", "nine9nine");
+        String rawInput = Input.INPUT;
+        rawInput = rawInput.replaceAll("one", "one1one");
+        rawInput = rawInput.replaceAll("two", "two2two");
+        rawInput = rawInput.replaceAll("three", "three3three");
+        rawInput = rawInput.replaceAll("four", "four4four");
+        rawInput = rawInput.replaceAll("five", "five5five");
+        rawInput = rawInput.replaceAll("six", "six6six");
+        rawInput = rawInput.replaceAll("seven", "seven7seven");
+        rawInput = rawInput.replaceAll("eight", "eight8eight");
+        rawInput = rawInput.replaceAll("nine", "nine9nine");
+        rawInput = rawInput.replaceAll("[a-z]+", "");
+        String[] inputs = rawInput.split("\n");
 
-            input = input.replaceAll("[a-z]", "");
+        long result = 0;
+        for (String input : inputs) {
             result += Long.parseLong(input.charAt(0) + "") * 10 + Long.parseLong(input.charAt(input.length() - 1) + "");
         }
         System.out.println(result);
