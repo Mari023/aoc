59748aec28256447de641106641856cd72621146
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-12-14 23:27:44 +0100

    move some stuff to RockField

--- main/src/year2023/day14/Day14.java
+++ main/src/year2023/day14/Day14.java
@@ -2,34 +2,28 @@ package year2023.day14;
 
 import util.TimedTest;
 
-import java.util.regex.Pattern;
-
 public class Day14 {
-    private static final Pattern NOTROCK = Pattern.compile("[#.]+");
-    private static final int CYCLE_LENGTH = 2 * 2 * 3 * 3 * 5 * 5 * 7;
+    private static final int CYCLE_LENGTH = 2 * 2 * 3 * 3 * 5 * 5 * 7 * 11;
     private static final int TOTAL_CYCLES = 1000000000;
 
     public static void main(String[] args) {
         var rocks = new RockField(Input.INPUT);
-        rollUp(rocks);
+        rocks.rollUp();
 
         if (TimedTest.PRINT) System.out.println(rocks);
-        System.out.println(calculateNorthLoad(rocks));
+        System.out.println(rocks.calculateNorthLoad());
 
 
-        rollLeft(rocks);
+        rocks.rollLeft();
         if (TimedTest.PRINT) {
-            System.out.println();
             System.out.println(rocks);
         }
-        rollDown(rocks);
+        rocks.rollDown();
         if (TimedTest.PRINT) {
-            System.out.println();
             System.out.println(rocks);
         }
-        rollRight(rocks);
+        rocks.rollRight();
         if (TimedTest.PRINT) {
-            System.out.println();
             System.out.println(rocks);
             System.out.println();
         }
@@ -37,115 +31,15 @@ public class Day14 {
             var copy = new RockField(rocks);
             int j = 0;
             for (; j < CYCLE_LENGTH && i + j < TOTAL_CYCLES; j++) {
-                rollUp(rocks);
-                rollLeft(rocks);
-                rollDown(rocks);
-                rollRight(rocks);
+                rocks.rollUp();
+                rocks.rollLeft();
+                rocks.rollDown();
+                rocks.rollRight();
             }
             if (rocks.equals(copy)) break;
         }
 
         if (TimedTest.PRINT) System.out.println(rocks);
-        System.out.println(calculateNorthLoad(rocks));
-    }
-
-    private static long calculateNorthLoad(RockField rocks) {
-        long load = 0;
-        for (int i = 0; i < rocks.getRows(); i++) {
-            load += (long) NOTROCK.matcher(new String(rocks.getRow(i))).replaceAll("").length() * (rocks.getRows() - i);
-        }
-        return load;
-    }
-
-    private static void rollUp(RockField rocks) {
-        for (int i = 1; i < rocks.getRows(); i++) {
-            for (int j = 0; j < rocks.getColumns(); j++) {
-                int y = i;
-                while (rollUp(rocks, j, y)) {
-                    y--;
-                }
-            }
-        }
-    }
-
-    private static boolean rollUp(RockField rocks, int x, int y) {
-        if (y == 0) return false;
-
-        if (rocks.get(x, y) == 'O' && rocks.get(x, y - 1) == '.') {
-            rocks.set(x, y, '.');
-            rocks.set(x, y - 1, 'O');
-            return true;
-        }
-        return false;
-    }
-
-    private static void rollDown(RockField rocks) {
-        for (int i = rocks.getRows() - 2; i >= 0; i--) {
-            for (int j = 0; j < rocks.getColumns(); j++) {
-                int y = i;
-                while (rollDown(rocks, j, y)) {
-                    y++;
-                }
-            }
-        }
-    }
-
-    private static boolean rollDown(RockField rocks, int x, int y) {
-        if (y == rocks.getRows() - 1) return false;
-
-        if (rocks.get(x, y) == 'O' && rocks.get(x, y + 1) == '.') {
-            rocks.set(x, y, '.');
-            rocks.set(x, y + 1, 'O');
-            return true;
-        }
-        return false;
-    }
-
-
-    private static void rollLeft(RockField rocks) {
-        for (int j = 1; j < rocks.getColumns(); j++) {
-            for (int i = 0; i < rocks.getRows(); i++) {
-                int x = j;
-                while (rollLeft(rocks, x, i)) {
-                    x--;
-                }
-            }
-        }
-    }
-
-
-    private static boolean rollLeft(RockField rocks, int x, int y) {
-        if (x == 0) return false;
-
-        if (rocks.get(x, y) == 'O' && rocks.get(x - 1, y) == '.') {
-            rocks.set(x, y, '.');
-            rocks.set(x - 1, y, 'O');
-            return true;
-        }
-        return false;
-    }
-
-
-    private static void rollRight(RockField rocks) {
-        for (int j = rocks.getColumns() - 2; j >= 0; j--) {
-            for (int i = 0; i < rocks.getRows(); i++) {
-                int x = j;
-                while (rollRight(rocks, x, i)) {
-                    x++;
-                }
-            }
-        }
-    }
-
-
-    private static boolean rollRight(RockField rocks, int x, int y) {
-        if (x == rocks.getColumns() - 1) return false;
-
-        if (rocks.get(x, y) == 'O' && rocks.get(x + 1, y) == '.') {
-            rocks.set(x, y, '.');
-            rocks.set(x + 1, y, 'O');
-            return true;
-        }
-        return false;
+        System.out.println(rocks.calculateNorthLoad());
     }
 }
--- main/src/year2023/day14/RockField.java
+++ main/src/year2023/day14/RockField.java
@@ -2,12 +2,116 @@ package year2023.day14;
 
 import util.CharArray2D;
 
+import java.util.regex.Pattern;
+
 public class RockField extends CharArray2D {
+    private static final Pattern NOTROCK = Pattern.compile("[#.]+");
+
     public RockField(RockField array) {
         super(array);
     }
 
     public RockField(String[] strings) {
         super(strings);
+    }
+
+    public long calculateNorthLoad() {
+        long load = 0;
+        for (int i = 0; i < getRows(); i++) {
+            load += (long) NOTROCK.matcher(new String(getRow(i))).replaceAll("").length() * (getRows() - i);
+        }
+        return load;
+    }
+
+    public void rollUp() {
+        for (int i = 1; i < getRows(); i++) {
+            for (int j = 0; j < getColumns(); j++) {
+                int y = i;
+                while (rollUp(j, y)) {
+                    y--;
+                }
+            }
+        }
+    }
+
+    private boolean rollUp(int x, int y) {
+        if (y == 0) return false;
+
+        if (get(x, y) == 'O' && get(x, y - 1) == '.') {
+            set(x, y, '.');
+            set(x, y - 1, 'O');
+            return true;
+        }
+        return false;
+    }
+
+    public void rollDown() {
+        for (int i = getRows() - 2; i >= 0; i--) {
+            for (int j = 0; j < getColumns(); j++) {
+                int y = i;
+                while (rollDown(j, y)) {
+                    y++;
+                }
+            }
+        }
+    }
+
+    private boolean rollDown(int x, int y) {
+        if (y == getRows() - 1) return false;
+
+        if (get(x, y) == 'O' && get(x, y + 1) == '.') {
+            set(x, y, '.');
+            set(x, y + 1, 'O');
+            return true;
+        }
+        return false;
+    }
+
+
+    public void rollLeft() {
+        for (int j = 1; j < getColumns(); j++) {
+            for (int i = 0; i < getRows(); i++) {
+                int x = j;
+                while (rollLeft(x, i)) {
+                    x--;
+                }
+            }
+        }
+    }
+
+
+    private boolean rollLeft(int x, int y) {
+        if (x == 0) return false;
+
+        if (get(x, y) == 'O' && get(x - 1, y) == '.') {
+            set(x, y, '.');
+            set(x - 1, y, 'O');
+            return true;
+        }
+        return false;
+    }
+
+
+    public void rollRight() {
+        for (int j = getColumns() - 2; j >= 0; j--) {
+            for (int i = 0; i < getRows(); i++) {
+                int x = j;
+                while (rollRight(x, i)) {
+                    x++;
+                }
+            }
+        }
+    }
+
+
+    private boolean rollRight(int x, int y) {
+        if (x == getColumns() - 1) return false;
+
+        if (get(x, y) == 'O' && get(x + 1, y) == '.') {
+            set(x, y, '.');
+            set(x + 1, y, 'O');
+            return true;
+        }
+        return false;
     }
 }
