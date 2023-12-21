3ee4d0f2a747aed98ceb651230531ab07fe449eb
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-12-21 10:47:58 +0100

    add toSting() method to Pulse

--- main/src/year2023/day20/components/Pulse.java
+++ main/src/year2023/day20/components/Pulse.java
@@ -1,6 +1,11 @@
 package year2023.day20.components;
 
 public record Pulse(Type type, Component sender, String receiver) {
+    @Override
+    public String toString() {
+        return "Pulse{type=" + type + ", sender=" + sender.name() + ", receiver=" + receiver + '}';
+    }
+
     public enum Type {
         HIGH, LOW
     }
