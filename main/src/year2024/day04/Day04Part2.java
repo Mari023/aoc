eac9aa9e35eef25581a6bd908502546c2cfa7a38
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-12-05 12:00:13 +0100

    slightly simplify Day04 Part02

--- main/src/year2024/day04/Day04Part2.java
+++ main/src/year2024/day04/Day04Part2.java
@@ -7,17 +7,14 @@ public class Day04Part2 {
         String input = Input.INPUT;
         int i = input.split("\n")[0].length();
         input = input.replace('\n', '_');
-        Pattern[] patterns = new Pattern[4];
-        patterns[0] = Pattern.compile("(?=M(.M.{" + (i - 2) + "}.A..{" + (i - 2) + "}S.S))");
-        patterns[1] = Pattern.compile("(?=M(.S.{" + (i - 2) + "}.A..{" + (i - 2) + "}M.S))");
-        patterns[2] = Pattern.compile("(?=S(.M.{" + (i - 2) + "}.A..{" + (i - 2) + "}S.M))");
-        patterns[3] = Pattern.compile("(?=S(.S.{" + (i - 2) + "}.A..{" + (i - 2) + "}M.M))");
+        var pattern = Pattern.compile("(?=M(.M.{" + (i - 2) + "}.A..{" + (i - 2) + "}S.S))|" +
+                "(?=M(.S.{" + (i - 2) + "}.A..{" + (i - 2) + "}M.S))|" +
+                "(?=S(.M.{" + (i - 2) + "}.A..{" + (i - 2) + "}S.M))|" +
+                "(?=S(.S.{" + (i - 2) + "}.A..{" + (i - 2) + "}M.M))");
         long XMAS = 0;
-        for (Pattern pattern : patterns) {
-            var matcher = pattern.matcher(input);
-            while (matcher.find()) {
-                XMAS++;
-            }
+        var matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            XMAS++;
         }
         System.out.println(XMAS);
     }
