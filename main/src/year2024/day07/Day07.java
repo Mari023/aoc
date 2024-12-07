c0e0c123766c13f34ef0e66fe1aa4649545b0ecc
Mari023 <38946771+Mari023@users.noreply.github.com>
2024-12-07 22:28:20 +0100

    Day07 Part 2

--- main/src/year2024/day07/Day07.java
+++ main/src/year2024/day07/Day07.java
@@ -1,10 +1,23 @@
 package year2024.day07;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day07 {
     public static void main() {
         long result = 0;
+        List<Equation> equations = new ArrayList<>();
         for (String s : Input.INPUT.split("\n")) {
             var e = Equation.fromString(s);
+            equations.add(e);
+            if (e.isValid()) {
+                result += e.result();
+            }
+        }
+        System.out.println(result);
+        Equation.part2 = true;
+        result = 0;
+        for (Equation e : equations) {
             if (e.isValid()) {
                 result += e.result();
             }
--- main/src/year2024/day07/Equation.java
+++ main/src/year2024/day07/Equation.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 public record Equation(long result, List<Integer> values) {
+    public static boolean part2 = false;
     public static final Map<Equation, Long> workingOperators = new HashMap<>();
+
     public boolean isValid() {
         long sum = 0;
         long product = 1;
@@ -14,13 +16,19 @@ public record Equation(long result, List<Integer> values) {
             sum += value;
             product *= value;
         }
-        //if (product < result || sum > result) return false;
-        if (product == result || sum == result) return true;
+        if (product == result || sum == result) {
+            workingOperators.put(this, -1L);
+            return true;
+        }
 
         if (values().size() > 65) throw new IllegalStateException("Can't work with more than 64 operators");
+        else if (part2 && values().size() > 33)
+            throw new IllegalStateException("Can't work with more than 33 operators");
         long maxOperator = 1L << (values.size() - 1);
+        if (part2)
+            maxOperator = 1L << ((values.size() - 1) * 2);
         for (long operators = 0; operators < maxOperator; operators++) {
-            if (test(operators)) {
+            if ((!part2 && test(operators)) || (part2 && test2(operators))) {
                 workingOperators.put(this, operators);
                 return true;
             }
@@ -39,12 +47,28 @@ public record Equation(long result, List<Integer> values) {
         return new Equation(result, List.copyOf(values));
     }
 
-    public boolean test(long operators) {
+    private boolean test(long operators) {
         long l = values().getFirst();
         for (int i = 1; i < values().size(); i++) {
             if (((operators >> (i - 1)) & 1) == 0)
                 l += values().get(i);
             else l *= values().get(i);
+        }
+        return result() == l;
+    }
+
+    private boolean test2(long operators) {
+        long l = values().getFirst();
+        for (int i = 1; i < values().size(); i++) {
+            int nextValue = values().get(i);
+            switch (((operators >> (2 * (i - 1))) & 0b11)) {
+                case 0L -> l += nextValue;
+                case 1L -> l *= nextValue;
+                case 2L -> l = Long.parseLong(l + "" + nextValue);
+                default -> {
+                    return false;
+                }
+            }
         }
         return result() == l;
     }
